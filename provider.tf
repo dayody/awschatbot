c27ab95b7961aab1f1b8b032dea90fb7ac507492
Adedayo Ademuyiwa <59888003+dayody@users.noreply.github.com>
2024-12-29 10:03:04 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,139 +4,152 @@ provider "aws" {
 
 resource "aws_vpc" "main" {
   cidr_block = "192.168.0.0/16"
-  tags = {
-    Name = "chatbot-vpc"
-  }
 }
 
-resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "192.168.1.0/24"
-  availability_zone       = "eu-west-2a"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "chatbot-subnet"
-  }
+resource "aws_subnet" "subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "192.168.1.0/24"
 }
 
-resource "aws_internet_gateway" "main" {
+resource "aws_security_group" "frontend_sg" {
   vpc_id = aws_vpc.main.id
-  tags = {
-    Name = "chatbot-igw"
-  }
-}
 
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
-  tags = {
-    Name = "chatbot-rt"
-  }
-}
-
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_security_group" "ecs" {
-  vpc_id = aws_vpc.main.id
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  tags = {
-    Name = "ecs-sg"
+}
+
+resource "aws_security_group" "backend_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
+resource "aws_ecr_repository" "frontend_repo" {
+  name = "frontend"
+}
+
+resource "aws_ecr_repository" "backend_repo" {
+  name = "backend"
+}
+
 resource "aws_ecs_cluster" "main" {
-  name = "chabot-cluster"
+  name = "chatbot-cluster"
 }
 
 resource "aws_iam_role" "ecs_task_execution_role" {
   name = "ecsTaskExecutionRole"
+
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
+    Version = "2012-10-17"
     Statement = [{
-      Effect = "Allow",
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
       Principal = {
         Service = "ecs-tasks.amazonaws.com"
-      },
-      Action = "sts:AssumeRole"
+      }
     }]
   })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
+  ]
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-resource "aws_ecs_task_definition" "chatbot_task" {
-  family                   = "chatbot-task"
+resource "aws_ecs_task_definition" "frontend_task" {
+  family                   = "frontend-task"
   network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]  # Changed to FARGATE
+  requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-
   container_definitions = jsonencode([{
-    name      = "chatbot-app"
-    image     = "851725211248.dkr.ecr.eu-west-2.amazonaws.com/chabot:53318906e7a3655037468475b704726e9a5faab8"
-    essential = true
-    portMappings = [{
+    name             = "frontend"
+    image            = "${aws_ecr_repository.frontend_repo.repository_url}:latest"
+    essential        = true
+    portMappings     = [{
       containerPort = 80
       hostPort      = 80
     }]
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        awslogs-group         = "/ecs/frontend"
+        awslogs-region        = "eu-west-2"
+        awslogs-stream-prefix = "ecs"
+      }
+    }
   }])
 }
 
-resource "aws_ecs_service" "chabot_service" {
-  name            = "chabot-service"
-  cluster         = aws_ecs_cluster.main.id
-  task_definition = aws_ecs_task_definition.chabot_task.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"  # Changed to FARGATE
+resource "aws_ecs_task_definition" "backend_task" {
+  family                   = "backend-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  container_definitions = jsonencode([{
+    name             = "backend"
+    image            = "${aws_ecr_repository.backend_repo.repository_url}:latest"
+    essential        = true
+    portMappings     = [{
+      containerPort = 5000
+      hostPort      = 5000
+    }]
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        awslogs-group         = "/ecs/backend"
+        awslogs-region        = "eu-west-2"
+        awslogs-stream-prefix = "ecs"
+      }
+    }
+  }])
+}
 
+resource "aws_ecs_service" "frontend_service" {
+  name            = "frontend-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.frontend_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
   network_configuration {
-    subnets          = [aws_subnet.public_subnet.id]
-    security_groups  = [aws_security_group.ecs.id]
-    assign_public_ip = true
+    subnets         = [aws_subnet.subnet.id]
+    security_groups = [aws_security_group.frontend_sg.id]
+  }
+}
+
+resource "aws_ecs_service" "backend_service" {
+  name            = "backend-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.backend_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets         = [aws_subnet.subnet.id]
+    security_groups = [aws_security_group.backend_sg.id]
   }
 }
